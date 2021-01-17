8491f76f56762140132f3a62672bab84fe0886d2
npc-vicki <vicki@newspaperclub.com>
2021-01-17 17:01:52 +0000

    Rename workout.routes to workouts.routes to match the code

---
+++ server/router/workouts.routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { to } from "await-to-js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { type, duration } = req.body;
+  console.log(`Type: ${type} Duration: ${duration}`);
+
+  /**
+   * In here, have a look at the router.post('register') example on line 43 of auth.route.js
+   * Have a think about how you could take some of that code and convert it to create a WORKOUT instead of a USER
+   */
+
+  return res
+    .status(200)
+    .json({
+      success: true,
+      data: "/",
+    });
+});
+
+export default router;
