59198ac973a2bc9c1b8d89164125fed9233120f7
npc-vicki <vicki@newspaperclub.com>
2021-01-24 14:45:53 +0000

    Set up workout to fetch workout data from an api
    Co-Authored-By: Shari-LW <68791989+shari-LW@users.noreply.github.com>

--- pages/workouts/[id].jsx
+++ pages/workouts/[id].jsx
@@ -1,14 +1,41 @@
 import React from "react";
 import { useRouter } from 'next/router'
 
-const Workout = () => {
+const Workout = ({ name }) => {
+  console.log(name);
   const router = useRouter()
   const { id } = router.query;
   return (
     <main>
       <div>ID: {id}</div>
+      <div>Name: {name}</div>
     </main>
   )
 };
+
+// TODO: vicki to debug next to figure out how to make this path dynamic
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: '1' } },
+    ],
+    fallback: false
+  };
+}
+
+export async function getStaticProps(context) {
+  const res = await fetch(`https://swapi.dev/api/people/1/`)
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { name: data.name }
+  }
+}
 
 export default Workout;
