c668adac0b1105b47fec37a5f823cbb0e4913170
npc-vicki <vicki@newspaperclub.com>
2021-01-24 17:47:21 +0000

    Create exercises.js
    Co-Authored-By: Shari-LW <68791989+shari-LW@users.noreply.github.com>

---
+++ data/exercises.js
@@ -0,0 +1,82 @@
+export default {
+  upperBody: [
+    { 
+      name: 'pushUp',
+      teachingPoints: [
+        'Position yourself in a plank position and slowly bend elbows until they are at 90 degrees',
+        'Return to starting position',
+      ]
+    },
+    { 
+      name: 'shoulderTap',
+      teachingPoints: [
+        'Start in plank position with feet hip width apart',
+        'Touch your left shoulder with right hand and return to plank position',
+        'Touch your right shoulder with left hand and return to plank position',
+        'Return to starting position',
+      ]
+    },
+    {
+      name: 'punch',
+      teachingPoints: [
+        'Stand with feet hip-width apart and alternate punches in the air'
+      ]
+    },
+  ],
+  lowerBody: [
+    { 
+      name: 'squat',
+      teachingPoints: [
+        'Stand with feet hip-width apart',
+        'Squat until knees at 90 degrees',
+        'Return to starting position',
+      ]
+    },
+    {
+      name: 'reverseLunge',
+      teachingPoints: [
+        'Stand upright with hands on hips',
+        'Take a step backwards with alternating feet',
+        'Lower your hips until your thigh is parallel to the floor'
+      ]
+    },
+    {
+      name: 'donkeyKicks',
+      teachingPoints: [
+        'Position yourself on all fours',
+        'Kick right leg backwards and return to starting position',
+        'Alternate with other leg'
+      ]
+    },
+  ],
+  core: [
+    {
+      name: 'plank',
+      teachingPoints: [
+        'Position yourself on elbows with legs extended straight',
+        'Keep a neutral spine while holding that position'
+      ]
+    },
+    {
+      name: 'reverseCrunch',
+      teachingPoints: [
+        'Lie flat on your back with your arms beside your body',
+        'Lift your feet and pull your knees back until thighs are perpendiculr to the floor',
+        'Return to starting position'
+      ]
+    },
+    {
+      name: 'russianTwist',
+      teachingPoints: [
+        'Sit with bent knees on the floor with hands at chest level',
+        'Alternate twisting left and right, keeping your spine straight'
+      ]
+    },
+  ]
+}
+
+// TODO: Add exercise images
+
+// TODO: come back to modeling this later
+// fullBody: [
+// ]
