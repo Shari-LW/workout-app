07855d6829fc07f502859f8d35c91657c0a047b8
Shari-LW <68791989+Shari-LW@users.noreply.github.com>
2021-01-24 13:26:28 +0000

    Add ID jsx

---
+++ pages/workouts/[id].jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { server } from "../../utils";
+
+const Workout = ({ query }) => {
+  console.log(query);
+  return <main>Hello</main>;
+};
+
+export default Workout;
