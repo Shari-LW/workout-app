bb8f63e9158a5af7e7b976fc16c788fc47000c29
npc-vicki <vicki@newspaperclub.com>
2021-01-31 13:27:43 +0000

    Remove duplicate line from schema index
    Co-Authored-By: Shari-LW <68791989+Shari-LW@users.noreply.github.com>

--- server/database/schema/index.js
+++ server/database/schema/index.js
@@ -2,5 +2,4 @@ import { UserModel } from "./user";
 import { WorkoutModel } from "./workout";
 import { ExerciseModel } from "./exercise";
 
-export { UserModel, WorkoutModel };
 export { ExerciseModel, UserModel, WorkoutModel };
