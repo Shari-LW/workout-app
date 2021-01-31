b9316d0a50baf9760c69219458b1f1742e53ceb8
Shari-LW <68791989+Shari-LW@users.noreply.github.com>
2021-01-31 12:38:04 +0000

    Changes to create.js and index.jw

--- server/database/exercise/create.js
+++ server/database/exercise/create.js
@@ -1,11 +1,12 @@
 import { ExerciseModel } from "../schema";
 
-async function createExercise({ name, teachingPoints }) {
+async function createExercise({ name, teachingPoints, workoutType }) {
   return new Promise(async (resolve, reject) => {
     return resolve(
       await ExerciseModel.create({
         name,
         teachingPoints,
+        workoutType,
       })
     );
   });
--- server/database/schema/index.js
+++ server/database/schema/index.js
@@ -1,4 +1,6 @@
 import { UserModel } from "./user";
 import { WorkoutModel } from "./workout";
+import { ExerciseModel } from "./exercise";
 
 export { UserModel, WorkoutModel };
+export { ExerciseModel, UserModel, WorkoutModel };
